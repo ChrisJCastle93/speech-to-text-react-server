883e79ce5b3919de3fc94edee89c93453d76e856
Chris Castle <chrisjcastle93@gmail.com>
2022-05-10 18:56:41 +0200

    updated package

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
