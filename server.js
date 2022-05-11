72a0bbc1e96be422890d2607de08be33162291fc
Chris Castle <chrisjcastle93@gmail.com>
2022-05-11 06:27:51 +0200

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const app = require("./app");
-const express = require('express')
-const path = require('path')
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
-app.use("/static", express.static(path.join(__dirname, "client/build")));
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
